fa69064a5c8e53470478f410b539fb3a44f11cb1
Devansh Sharma <ds16022004@gmail.com>
2024-02-05 19:14:09 +0000

    3rd commit

--- tango_with_django_project/tango_with_django_project/rango/views.py
+++ tango_with_django_project/tango_with_django_project/rango/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render#
+
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    about_link = '<a href="/rango/about/">About page</a>'
+    return HttpResponse(f"Rango says hey there partner! Check out the {about_link}.")
 
 
+
+
+def about(request):
+    index_link = '<a href="/rango/">Index page</a>'
+    return HttpResponse(f"Rango says here is the about page. Go back to the {index_link}.")
